c42dcb2e34b30ff68eda827f933564be6abd06d5
F1Lse <118463105+F1Lse@users.noreply.github.com>
2024-03-13 21:48:30 +0800

    更换新版地盘代码

--- chassis+rudder/app/chassis_task.c
+++ chassis+rudder/app/chassis_task.c
@@ -51,10 +51,10 @@ void chassis_init()
 //		chassis.rudder_ecd_offset[3] = 3880;//6500
 		
 				
-		chassis.rudder_ecd_offset[0] = 5050;//100
-		chassis.rudder_ecd_offset[1] = 3597;//5300
-		chassis.rudder_ecd_offset[2] = 6253;//6350
-		chassis.rudder_ecd_offset[3] = 6668;//6500
+		chassis.rudder_ecd_offset[0] = 2173;//100
+		chassis.rudder_ecd_offset[1] = 5329;//5300
+		chassis.rudder_ecd_offset[2] = 6356;//6350
+		chassis.rudder_ecd_offset[3] = 3766;//6500
 		
 		
 		
